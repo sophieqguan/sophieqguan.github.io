d9ef4b27d88724cda6599dabb9562eb8c9ce14f6
soap <sophie.qin.guan@gmail.com>
2020-06-08 12:26:33 -0700

    made constructors

--- codified/src/codified/Codified.java
+++ codified/src/codified/Codified.java
@@ -1,6 +1,21 @@
 package codified;
 
 public class Codified {
+	private String nom;
+	
+	public Codified(String nom) {
+		super();
+		this.nom = nom;
+	}
+	
+	
+
+	public Codified() {
+		super();
+	}
+
+
+
 	public static void main(String[] args) {
 		System.out.println("apples");
 	}
